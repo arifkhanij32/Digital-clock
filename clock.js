a89d6d348d4ebb189d9ce0c9dd8bb1e3c80e2ee5
arifkhanij32 <159407966+arifkhanij32@users.noreply.github.com>
2024-06-26 22:11:51 +0530

    digital clock

---
+++ clock.js
@@ -0,0 +1,12 @@
+function Time(){
+    let now=new Date();
+    let hours=now.getHours().toString().padStart(2,"0")
+    let minutes=now.getMinutes().toString().padStart(2,"0")
+    let seconds=now.getSeconds().toString().padStart(2,"0")
+    const content=`${hours}:${minutes}:${seconds}`
+    document.getElementById("time").textContent=content
+
+
+}
+setInterval(Time,1000)
+Time();
